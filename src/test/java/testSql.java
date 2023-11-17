9a76439de3405dd1e5f052d0de82ce516726bc3c
Jay <1030341806@qq.com>
2023-11-17 13:24:51 +0800

    testSql

--- src/test/java/testSql.java
+++ src/test/java/testSql.java
@@ -4,9 +4,15 @@ public class testSql {
     public static void main(String[] args) {
         try {
             SqlFunction.showGrants("root");
+            SqlFunction.showDataBases();
+            SqlFunction.showTables();
+            SqlFunction.showUser();
+            SqlFunction.showdatabase();
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
+
+
     }
 }
